1283f0c24320029f421d2b5945eb6295666abf4d
didim99 <didim@eclabs.ru>
2019-12-12 19:01:27 +0300

    > Migrated to AndroidX

--- library/src/main/java/ru/didim99/library/ui/dirpicker/DirListAdapter.java
+++ library/src/main/java/ru/didim99/library/ui/dirpicker/DirListAdapter.java
@@ -2,13 +2,13 @@ package ru.didim99.library.ui.dirpicker;
 
 import android.content.Context;
 import android.content.res.Resources;
-import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 import ru.didim99.library.R;
 
